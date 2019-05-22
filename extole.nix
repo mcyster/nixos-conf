94e166ee85411cabbc690b5b7eefdd305072ddf4
Mark Cyster <mcyster@extole.com>
2019-05-22 13:40:00 -0700

    Match extole template

--- extole.nix
+++ extole.nix
@@ -43,4 +43,13 @@
   # these are ports necessary for nfs communication between lo and nixos
   networking.firewall.allowedTCPPorts = [ 2049 20048 111 ];
   networking.firewall.allowedUDPPorts = [ 20048 111 ];
+
+
+  # Deprecated use ~/extolerc.conf
+  #environment.variables.E_GITHUB_TOKEN = pkgs.lib.mkOverride 0 "YOUR_GITHUB_TOKEN";
+  #environment.variables.E_UID = pkgs.lib.mkOverride 0 "YOUR_UID";
+  #environment.variables.E_GID = pkgs.lib.mkOverride 0 "YOUR_GID";
+  #environment.variables.E_VM_NFS_OPTIONS = pkgs.lib.mkOverride 0 "vers=3,rw";
+  #environment.variables.E_VM_MEMORY = pkgs.lib.mkOverride 0 "NUMBER_OF_MEGABYTES_HERE";
+  #environment.variables.E_VM_CPUS = pkgs.lib.mkOverride 0 "NUMBER_OF_CORES";
 }
